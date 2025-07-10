29e6ed32bfd1f4fa9c746e1f14aca8e465e40a69
rehanpratama12 <komitmendirisendiri@gmail.com>
2025-07-10 16:34:56 +0700

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,128 @@
+const logotext = "REY";
+const meta = {
+  title: "Rehan Pratama Pasaribu",
+  description: "Halo! Saya Rehan Pratama Pasaribu, seorang Desainer UI/UX yang siap mewujudkan ide-ide cemerlang, saat ini berkreasi di Medan, Indonesia.",
+};
+
+const introdata = {
+  title: "Halo, Saya Rehan Pratama Pasaribu!",
+  animated: {
+    first: "Saya suka merancang pengalaman",
+    second: "Saya membuat antarmuka yang memukau",
+    third: "Saya mengubah ide jadi visual!",
+  },
+  description: "Seorang Desainer UI/UX dengan semangat membara untuk menciptakan pengalaman digital yang tidak hanya fungsional, tapi juga menyenangkan dan memanjakan mata. Mari kita wujudkan desain impian Anda!",
+  your_img_url: "https://serayunews.pw/wp-content/uploads/2025/04/Don-Film-Jumbo.jpg",
+};
+
+const dataabout = {
+  title: "Mari berkenalan lebih dekat!",
+  aboutme:
+    "Sebagai Desainer UI/UX, saya adalah jembatan antara keinginan pengguna dan tujuan bisnis yang ingin Anda capai. Saya ahli dalam menggali kebutuhan user, membuat sketsa awal (wireframing), menciptakan prototipe interaktif, hingga menghasilkan desain visual yang bikin betah berlama-lama. Selalu haus akan tren desain terbaru dan tools canggih, saya siap memberikan pengalaman pengguna terbaik untuk proyek Anda!",
+};
+
+const worktimeline = [
+  {
+    jobtitle: "Desainer UI/UX Junior",
+    where: "PT. Inovasi Kreatif",
+    date: "2023",
+  },
+  {
+    jobtitle: "Spesialis Desain Freelance",
+    where: "Bekerja dari Mana Saja",
+    date: "2022",
+  },
+  {
+    jobtitle: "Magang Desain Interaktif",
+    where: "Studio Desain Impian",
+    date: "2021",
+  },
+];
+
+const skills = [
+  {
+    name: "Figma",
+    value: 90,
+  },
+  {
+    name: "Adobe XD",
+    value: 85,
+  },
+  {
+    name: "Riset Pengguna",
+    value: 80,
+  },
+  {
+    name: "Wireframing & Prototyping",
+    value: 88,
+  },
+  {
+    name: "Desain Antarmuka (UI)",
+    value: 92,
+  },
+  {
+    name: "HTML/CSS Dasar",
+    value: 70,
+  },
+];
+
+const services = [
+  {
+    title: "Desain UI Memukau",
+    description: "Menciptakan antarmuka pengguna yang bukan cuma cantik, tapi juga super gampang digunakan di web dan aplikasi mobile Anda. Siap-siap terpukau!",
+  },
+  {
+    title: "Riset & Strategi UX Juara",
+    description: "Mari kita selami keinginan pengguna Anda! Saya akan riset, buat alur yang mulus, dan susun strategi terbaik untuk pengalaman pengguna yang tak terlupakan.",
+  },
+  {
+    title: "Prototyping & Pengujian Asyik",
+    description: "Mengembangkan prototipe interaktif yang bisa dicoba langsung, lalu kita uji bareng untuk memastikan desain kita benar-benar jadi favorit pengguna. Seru, kan?",
+  },
+];
+
+const dataportfolio = [
+  {
+    img: "https://miro.medium.com/v2/resize:fit:4800/format:webp/1*Ke4wO61g2-xuzOfazvbMNQ.png",
+    description: "Redesain Aplikasi Belanja Online: Membuat pengalaman belanja jadi lebih mulus dan visualnya lebih segar. Cek perubahan besar ini!",
+    link: "#",
+  },
+  {
+    img: "https://www-finereport-com.translate.goog/en/wp-content/uploads/2023/06/Designer-5.png?_x_tr_sl=en&_x_tr_tl=id&_x_tr_hl=id&_x_tr_pto=imgs",
+    description: "Konsep Dashboard Analitik Keuangan: Visualisasi data yang bersih dan interaktif, memudahkan pengambilan keputusan. Data jadi teman baik!",
+    link: "#",
+  },
+  {
+    img: "https://qreatif.id/image_apps/qreatifid-membaca-menulis.webp?",
+    description: "Aplikasi Edukasi Interaktif untuk Anak: Belajar jadi lebih asyik dengan desain penuh warna dan fitur yang ramah pengguna. Yuk, belajar sambil bermain!",
+    link: "#",
+  },
+  {
+    img: "https://cdnpro.eraspace.com/media/.renditions/wysiwyg/artikel/Tahun_2024/Desember/Aplikasiedukasi-1.jpg",
+    description: "Situs Web Portofolio Fotografer: Menampilkan karya seni visual dengan tata letak yang elegan dan navigasi yang intuitif. Abadikan momen indah dengan desain!",
+    link: "#",
+  },
+  {
+    img: "https://buildwithangga.com/storage/assets/thumbnail_tips/BuildWithAngga-thumbnail-tips-DNqr8-tutorial-bikin-ui-design-homepage-untuk-aplikasi-order-makanan-cover%20food.jpg",
+    description: "Desain Aplikasi Pemesanan Makanan Online: Cepat, mudah, dan bikin lapar! Antarmuka yang mulus dari pilih menu sampai pembayaran.",
+    link: "#",
+  },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "pratamabelajar160@gmail.com",
+  YOUR_FONE: "+62 858-3755-4819",
+  description: "Tertarik berkolaborasi atau sekadar ngobrolin desain? Jangan sungkan, langsung saja hubungi saya lewat email atau telepon. Saya selalu senang mendengar kabar dari Anda!",
+  YOUR_SERVICE_ID: "service_id",
+  YOUR_TEMPLATE_ID: "template_id",
+  YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+  github: "https://github.com/RehanPratama",
+  facebook: "https://facebook.com/RehanPratamaOfficial",
+  linkedin: "https://linkedin.com/in/rehanpratama-uiux",
+  twitter: "https://twitter.com/RehanPratama",
+};
+
+export { meta, dataabout, dataportfolio, worktimeline, skills, services, introdata, contactConfig, socialprofils, logotext };
